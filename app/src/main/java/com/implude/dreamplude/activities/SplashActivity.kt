b46aa9bccfbcaf99ba35c6b02d5cf8d8403037d5
SeungHyunMin <asdf014563@naver.com>
2019-09-27 20:38:29 +0900

    SplashActivity 추가

---
+++ app/src/main/java/com/implude/dreamplude/activities/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.implude.dreamplude.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.implude.dreamplude.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
