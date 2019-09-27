2afb67a03a271b942904a2dfc7e1771e176075ee
SeungHyunMin <asdf014563@naver.com>
2019-09-27 20:47:47 +0900

    스플래시 화면 1초로 설정

--- app/src/main/java/com/implude/dreamplude/activities/SplashActivity.kt
+++ app/src/main/java/com/implude/dreamplude/activities/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.implude.dreamplude.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.implude.dreamplude.R
 
@@ -9,5 +11,10 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1000)
     }
 }
