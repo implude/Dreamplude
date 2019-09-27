59a19e8009f6cbbc5b66df421e0fb34127e604eb
wkdwhddn06 <wkdwhddn06@naver.com>
2019-09-27 21:33:43 +0900

    splash theme 작성 및 적용

--- app/src/main/java/com/implude/dreamplude/activities/SplashActivity.kt
+++ app/src/main/java/com/implude/dreamplude/activities/SplashActivity.kt
@@ -4,13 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.implude.dreamplude.R
 
 class SplashActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
