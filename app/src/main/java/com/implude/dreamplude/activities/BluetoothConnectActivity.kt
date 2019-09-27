c62a6e1aabc7368e5ff2d574483b5aa04be24e20
SeungHyunMin <asdf014563@naver.com>
2019-09-27 21:10:05 +0900

    블루투스 액티비티 생성

---
+++ app/src/main/java/com/implude/dreamplude/activities/BluetoothConnectActivity.kt
@@ -0,0 +1,13 @@
+package com.implude.dreamplude.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.implude.dreamplude.R
+
+class BluetoothConnectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blutooth_connect)
+    }
+}
