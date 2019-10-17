7e1f732c3838c7d849c70e3ef76cabcafa328db9
SeungHyunMin <asdf014563@naver.com>
2019-10-17 10:06:32 +0900

    ACCESS_COARSE_LOCATION 권한 요청 추가

--- app/src/main/java/com/implude/dreamplude/view/bluetooth/utils/BluetoothRequest.kt
+++ app/src/main/java/com/implude/dreamplude/view/bluetooth/utils/BluetoothRequest.kt
@@ -1,12 +1,22 @@
 package com.implude.dreamplude.view.bluetooth.utils
 
+import android.Manifest
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import android.content.Context
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.implude.dreamplude.view.bluetooth.models.BluetoothStateViewModel
 
-class BluetoothRequest(private val context: Context, viewModel: BluetoothStateViewModel) {
+class BluetoothRequest(private val context: Activity, viewModel: BluetoothStateViewModel) {
+    init {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(context, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+        }
+    }
+
     private val bluetoothStateReceiver = BluetoothStateReceiver(viewModel)
 
     fun registerReceiver() = IntentFilter().run {
