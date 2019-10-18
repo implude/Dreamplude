55ef8178c4e03a6204aaae207dd4f8b90db832ac
StarBox <y2142144@gmail.com>
2019-10-18 21:05:30 +0900

    FunctionActivity Toolbar 추가, FunctionDetailActivity 생성

---
+++ app/src/main/java/com/implude/dreamplude/view/functiondetail/FunctionDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.implude.dreamplude.view.functiondetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FunctionDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_function_detail)
+    }
+}
