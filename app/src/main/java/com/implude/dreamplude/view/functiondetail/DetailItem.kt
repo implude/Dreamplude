2c6e0ac31791829ef008d8e1e01ad4afd08954c1
SeungHyunMin <asdf014563@naver.com>
2019-10-19 00:45:08 +0900

    FunctionDetailActivity 구현

---
+++ app/src/main/java/com/implude/dreamplude/view/functiondetail/DetailItem.kt
@@ -0,0 +1,3 @@
+package com.implude.dreamplude.view.functiondetail
+
+data class DetailItem(val title: String, val description: String)
--- app/src/main/java/com/implude/dreamplude/view/functiondetail/FunctionDetailActivity.kt
+++ app/src/main/java/com/implude/dreamplude/view/functiondetail/FunctionDetailActivity.kt
@@ -3,11 +3,21 @@ package com.implude.dreamplude.view.functiondetail
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.implude.dreamplude.R
+import kotlinx.android.synthetic.main.activity_function_detail.*
 
 class FunctionDetailActivity : AppCompatActivity() {
+    private val items = listOf(
+        DetailItem("음악 재생/중지", "음악을 재생하고 중지합니다."),
+        DetailItem("이전 음악", "이전 음악을 재생합니다."),
+        DetailItem("다음 음악", "다음 음악을 재생합니다.")
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_function_detail)
+
+        val position = intent.getIntExtra("position", 0)
+        titleText.text = items[position].title
+        descriptionText.text = items[position].description
     }
 }
